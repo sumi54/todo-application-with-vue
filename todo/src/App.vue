e7a671969c6fd407bd70a31ae3ebe067093d912f
sumi54 <sumeyyekarakullukcu34@gmail.com>
2023-07-26 17:17:58 +0300

    eklentileri ekleme ve jsonlaarı getirme

---
+++ todo/src/App.vue
@@ -0,0 +1,22 @@
+import { RouterLink, RouterView } from 'vue-router'
+
+<template>
+
+  <RouterView />
+</template>
+
+<style>
+body{
+  background-color: #2f3542;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #f1f2f6;
+  max-width: 800px;
+  margin: 0 auto;
+}
+</style>
---
+++ todo/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <div v-if="yapilacaklar.length">
+      <div v-for="yap in yapilacaklar" :key="yap.id">
+        <p>{{ yap.baslik }}</p>
+      </div>
+    </div>
+    <div v-else>
+      <p>Yapılacaklar Yükleniyor..</p>
+    </div>
+  </div>
+</template>
+<script>
+export default{
+  name:'Home',
+  data(){
+    return{
+      yapilacaklar:[]
+    }
+  },
+  mounted(){
+    fetch('http://localhost:3000/yapilacaklar')
+    .then((res)=>res.json())
+    .then((data)=>this.yapilacaklar=data)
+    .catch((err)=>console.log(err))
+  }
+}
+</script>
